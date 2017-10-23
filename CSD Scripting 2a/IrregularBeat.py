3a3f41991073d3066674e961510687397aef1004
dengsn <info@dengsn.com>
2017-10-23 12:11:11 +0200

    Added the options class and finished the UI

--- CSD Scripting 2a/IrregularBeat.py
+++ CSD Scripting 2a/IrregularBeat.py
@@ -1,6 +1,42 @@
 import simpleaudio as sa;
 import time;
 import random;
+import colorama;
+
+# Create a list of default values for user settings
+defaults = {
+  "samples": ["assets/808-Kick.wav","assets/808-Snare.wav","assets/808-OH.wav"],
+  "bpm": 120
+}
+
+# Option class for selecting things in the terminal
+class Option:
+  # Constructor
+  # Options is a list of strings
+  def __init__(self, options):
+    self.options = options
+    
+  # Let the user choose an option
+  def choose(self):
+    # Print the header
+    print(colorama.Fore.BLACK + colorama.Back.GREEN + "What would you like to do next?")
+    # Print all options
+    for index, option in enumerate(self.options):
+      print("  %d. %s" % (index + 1,option))
+    # Wait for input
+    inputInt = None
+    while inputInt == None:
+      try:
+        inputStr = input("Enter your choice: ")
+        inputInt = int(inputStr)
+        if inputInt <= 0 or inputInt > len(self.options):
+          raise ValueError
+        break
+      except ValueError:
+        print("Invalid")
+        inputInt = None
+    # Return the index of the selected option
+    return inputInt
 
 # Sampler class, extends list
 class Sampler(list):
@@ -121,61 +157,104 @@ def generate_patterns(length):
     # Add a random pattern
     currentLength = random.choice([2,3,4])
     return [currentLength] + generate_patterns(length - currentLength)
-
-# Main function
-def main():    
-  # Print welcome message
-  print("""
-  ___                       _          ___           _               
- |_ _|_ _ _ _ ___ __ _ _  _| |__ _ _ _| _ ) ___ __ _| |_   _ __ _  _ 
-  | || '_| '_/ -_) _` | || | / _` | '_| _ \/ -_) _` |  _|_| '_ \ || |
- |___|_| |_| \___\__, |\_,_|_\__,_|_| |___/\___\__,_|\__(_) .__/\_, |
-                 |___/                                    |_|   |__/ 
-  """)
+   
+# Generate a new beat with user input settings   
+def generate_beat():
+  # Print a newline
+  print("")
   
-  # Create a list of default values for user settings
-  defaults = {
-    "samples": ["assets/808-Kick.wav","assets/808-Snare.wav","assets/808-OH.wav"],
-    "bpm": 120
-  }
+  # Print the header
+  print(colorama.Fore.BLACK + colorama.Back.GREEN + "Enter the settings for the new beat (press ENTER for the default value)")
 
-  # Create a list to store the samples
-  sampler = Sampler()
-
-  # Ask for the samples to use
-  print("Enter the following settings to configure the script (press ENTER for the default value)")
-  for i in range(3):
-    while True:
-      # Ask for the sample to use and append it to the list
-      sampleFile = input("- Enter the path to sample #%d (%s): " % (i + 1, defaults["samples"][i]))
-      if sampler.append_file(sampleFile or defaults["samples"][i]):
-        break
-      else:
-        print("Could not fild the file! Please try again.")
-
+  # Ask for the time signature to use
+  try:
+    timeSignatureInput = input("  Enter the time signature (7/8): ")
+  except ValueError:
+    pass
+  
   # Ask for the bpm to use
   try:
-    bpmInput = input("- Enter the desired BPM (%d): " % defaults["bpm"])
+    bpmInput = input("  Enter the desired BPM (%d): " % defaults["bpm"])
     bpm = int(bpmInput or defaults["bpm"])
   except ValueError:
     print("Invalid input, using default BPM")
     bpm = defaults["bpm"]
+  
+  # Create a new sequencer and return it
+  beats = random.randrange(5,10)
+  return Sequencer.generate_irregular_beat(bpm,beats);
+  
+# Initialize a sampler with user input settings
+def init_sampler():
+  # Create a list to store the samples
+  sampler = Sampler()
+  
+  # Print a newline
+  print("")
+  
+  # Print the header
+  print(colorama.Fore.BLACK + colorama.Back.GREEN + "Enter the settings to configure the script (press ENTER for the default value)")
+
+  # Ask for the samples to use
+  for i in range(3):
+    while True:
+      # Ask for the sample to use and append it to the list
+      sampleFile = input("  Enter the path to sample #%d (%s): " % (i + 1, defaults["samples"][i]))
+      if sampler.append_file(sampleFile or defaults["samples"][i]):
+        break
+      else:
+        print(colorama.Fore.RED + "  Could not find the file! Please try again.")
+        
+  # Return the sampler
+  return sampler
+
+# Main function
+def main():    
+  # Initialize colorama
+  colorama.init(autoreset=True)
+
+  # Print welcome message
+  print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "  ___                       _          ___           _               ")
+  print(colorama.Fore.GREEN + colorama.Style.BRIGHT + " |_ _|_ _ _ _ ___ __ _ _  _| |__ _ _ _| _ ) ___ __ _| |_   _ __ _  _ ")
+  print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "  | || '_| '_/ -_) _` | || | / _` | '_| _ \/ -_) _` |  _|_| '_ \ || |")
+  print(colorama.Fore.GREEN + colorama.Style.NORMAL + " |___|_| |_| \___\__, |\_,_|_\__,_|_| |___/\___\__,_|\__(_) .__/\_, |")
+  print(colorama.Fore.GREEN + colorama.Style.NORMAL + "                 |___/                                    |_|   |__/ ")
+  print("")
+  print("  Generate beats with 2-3-4-patterns in irregular time signatures")
+  print("  Created by dengsn (https://github.com/dengsn)")
+  
+  # Initialize the sampler
+  sampler = init_sampler()
+  
+  # Initialize a new beat
+  seq = generate_beat()
 
   # Main loop
   notInterrupted = True
   while notInterrupted:
-    # Create a new sequencer
-    beats = random.randrange(5,10)
-    seq = Sequencer.generate_irregular_beat(bpm,beats);
-    
-    print("Sequence with %d beats" % beats)
-
     # Play the sequence using the sampler
     seq.play(sampler)
+    
+    # Print a newline
+    print("")
   
-    # Ask for input
-    if input("Enter someting to quit: "):
-      exit()
+    # Options
+    option = Option(["Save the beat to a MIDI file","Replay the current beat","Generate a new beat","Exit the application"]).choose()
+    if option == 1:
+      # Save the beat to a MIDI file
+      pass
+    elif option == 2:
+      # Replay the current beat -> just pass
+      pass
+    elif option == 3:
+      # Generate a new beat
+      seq = generate_beat()
+    elif option == 4:
+      # Exit the application
+      break
+  
+  # Deinitialize colorama
+  colorama.deinit()
     
 # Execute the main function if not importing the script
 if __name__ == "__main__":
